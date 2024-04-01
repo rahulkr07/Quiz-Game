7e34db88f1cef43efde3ba4503822871e626ecc3
Rahul Kumar <76441159+rahulkr07@users.noreply.github.com>
2024-04-01 23:05:47 +0530

    quiz_game.py

--- quiz_game.py
+++ quiz_game.py
@@ -21,18 +21,14 @@ questions = [
         "answer": "A"
     },
     {
-        "prompt": "Which language is primarily spoken in Brazil?",
-        "options": ["A. Spanish", "B. Portuguese", "C. English", "D. French"],
+        "prompt": "Year Jharkhand became a state?",
+        "options": ["A. 2001", "B. 2000", "C. 2003", "D. 1999"],
         "answer": "B"
     },
+    
     {
-        "prompt": "What is the smallest prime number?",
-        "options": ["A. 1", "B. 2", "C. 3", "D. 5"],
-        "answer": "B"
-    },
-    {
-        "prompt": "Who wrote 'To Kill a Mockingbird'?",
-        "options": ["A. Harper Lee", "B. Mark Twain", "C. J.K. Rowling", "D. Ernest Hemingway"],
+        "prompt": "Most Pilgrim City'?",
+        "options": ["A. Deoghar", "B.Dhanbad ", "C.BOKARO", "D. Jamshedpur"],
         "answer": "A"
     }
 ]
